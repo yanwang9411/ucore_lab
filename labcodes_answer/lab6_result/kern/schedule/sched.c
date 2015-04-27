76b9a1142dd07699bc5654fb6015972ca920deb6
yanwang9411 <wangyan19941102@163.com>
2015-04-27 14:25:06 +0800

    Update sched.c

--- labcodes_answer/lab6_result/kern/schedule/sched.c
+++ labcodes_answer/lab6_result/kern/schedule/sched.c
@@ -83,10 +83,12 @@ schedule(void) {
     {
         current->need_resched = 0;
         if (current->state == PROC_RUNNABLE) {
+            cprintf("switch from process %d\n", current->pid);
+
             sched_class_enqueue(current);
         }
         if ((next = sched_class_pick_next()) != NULL) {
-            cprintf("switch to process %d", next->pid);
+            cprintf(" to process %d\n", next->pid);
             sched_class_dequeue(next);
         }
         if (next == NULL) {
